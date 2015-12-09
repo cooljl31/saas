78a7c1e41f78c2a35ca75ea8df122f3ea1e7ea91
Jim Lesperance <lesperancejim.lj@hotmail.com>
2015-12-09 00:07:40 +0000

    initialisation AppRB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saas::Application.config.secret_token = '8974f320b7b20526fca10ffff25856ebd69f83237094e1b46aaf128969e3d7af3b9c2d52107584b7e0bf0c98b131c9d9e2d4d2415ef575dbdbdc1ed05a7c7726'
